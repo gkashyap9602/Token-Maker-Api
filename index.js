2024a55d108fe25acf6fc1394b9db3aebab0daa0
gkashyap9602 <gkashyap9602@gmail.com>
2022-10-21 20:06:45 +0530

    change cors3

--- index.js
+++ index.js
@@ -16,16 +16,32 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('views'));
+
+var allowedOrigins = ['http://localhost:3000',
+                      'https://tokenmaker.block-brew.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 // app.use('/static', express.static(path.join(__dirname, './views')));
 //app.use(multer().array())
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
- });
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+//  });
  
- app.use(cors({origin:"https://tokenmaker.block-brew.com"}));
+//  app.use(cors({origin:"https://tokenmaker.block-brew.com"}));
   app.use('/api/v1', route);
   
   app.use(middleware.errCreate)
